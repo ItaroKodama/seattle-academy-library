d79bbf2c31bb4d8fa84870862952ea2ff7c01698
Itaro <user@usernoMacBook-Pro.local>
2021-04-22 16:17:53 +0900

    fix task_7

--- src/main/java/jp/co/seattle/library/controller/EditBookController.java
+++ src/main/java/jp/co/seattle/library/controller/EditBookController.java
@@ -1,5 +1,9 @@
 package jp.co.seattle.library.controller;
 
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 import org.slf4j.Logger;
@@ -64,36 +68,47 @@ public class EditBookController {
             @RequestParam("author") String author,
             @RequestParam("publisher") String publisher,
             @RequestParam("thumbnail") MultipartFile file,
-            @RequestParam("publish_date") String publish_date,
+            @RequestParam("publish_date") String publishDate,
             @RequestParam("isbn") String isbn,
             @RequestParam("description") String description,
             @RequestParam("bookId") Integer bookId,
             Model model) {
         logger.info("Welcome delete! The client locale is {}.", locale);
 
-        // パラメータで受け取った書籍情報をDtoに格納する。
         BookDetailsInfo bookInfo = new BookDetailsInfo();
-        bookInfo.setTitle(title);
-        bookInfo.setAuthor(author);
-        bookInfo.setPublisher(publisher);
-        bookInfo.setPublish_date(publish_date);
-        bookInfo.setIsbn(isbn);
-        bookInfo.setDescription(description);
-        bookInfo.setBookId(bookId);
-
+        
         //出版日とISBNのバリデーションチェック
         boolean flag = false;
-        if (!(bookInfo.getPublish_date().matches("(19[0-9]{2}|20[0-9]{2})(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])"))) {
-            model.addAttribute("notDateError", "出版日はYYYYMMDDの形式で入力してください");
+        if (!(publishDate.matches("[0-9]{8}"))) {
+            model.addAttribute("notDateError", "出版日はYYYYMMDDの形式で入力してください1");
             flag = true;
+        } else {
+            try {
+                DateFormat df = new SimpleDateFormat("yyyyMMdd");
+                df.setLenient(false);
+                df.parse(publishDate); // df.parseでParseExceptionがThrowされる                
+            } catch (ParseException p) {
+                model.addAttribute("notDateError", "出版日はYYYYMMDDの形式で入力してください2");
+                flag = true;
+                p.printStackTrace();
+            }
         }
-        if (!(bookInfo.getIsbn().matches("([0-9]{10}|[0-9]{13})?"))) {
+        if (!(isbn.matches("([0-9]{10}|[0-9]{13})?"))) {
             model.addAttribute("notISBNError", "ISBNは10桁もしくは13桁の数字で入力してください");
             flag = true;
         }
         if (flag) {
             return "editBook";
         }
+        
+        // パラメータで受け取った書籍情報をDtoに格納する。
+        bookInfo.setTitle(title);
+        bookInfo.setAuthor(author);
+        bookInfo.setPublisher(publisher);
+        bookInfo.setPublish_date(publishDate);
+        bookInfo.setIsbn(isbn);
+        bookInfo.setDescription(description);
+        bookInfo.setBookId(bookId);
 
         // クライアントのファイルシステムにある元のファイル名を設定する
         String thumbnail = file.getOriginalFilename();
